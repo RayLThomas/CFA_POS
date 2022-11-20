c3486cc9941282c94bcbae5ac3a54867492a5b2d
Raymond Thomas <raythomasclient@gmail.com>
2022-11-19 20:49:33 -0500

    added input order / ai order checker logic and animations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.getElementsByTagName('body')[0];
 const pos = document.getElementsByClassName("pos_display")[0];
 const ai_order_block = document.getElementsByClassName("ai_order")[0];
 const dine_btn = document.getElementById("dine_in_btn");
@@ -59,6 +60,52 @@ const menu = {
   },
 };
 
+let _inputed_Order = [];
+
+const replace_with_space = (s) => s.split("_").join(" ");
+const replace_ = (s) => s.split("_").join("");
+const replace_hyphen = (s) => s.split("-").join("");
+
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+  // if the other array is a falsy value, return
+  if (!array)
+      return false;
+  // if the argument is the same array, we can be sure the contents are same as well
+  if(array === this)
+      return true;
+  // compare lengths - can save a lot of time 
+  if (this.length != array.length)
+      return false;
+
+  for (var i = 0, l=this.length; i < l; i++) {
+      // Check if we have nested arrays
+      if (this[i] instanceof Array && array[i] instanceof Array) {
+          // recurse into the nested arrays
+          if (!this[i].equals(array[i]))
+              return false;       
+      }           
+      else if (this[i] != array[i]) { 
+          // Warning - two different object instances will never be equal: {x:20} != {x:20}
+          return false;   
+      }           
+  }       
+  return true;
+}
+
+const tryAgain = () => {
+  body.classList.add("tryAgainAnimation");
+  setTimeout(() => {
+    body.classList.remove("tryAgainAnimation");
+  }, "500");  
+}
+const perfect = () => {
+  body.classList.add("perfectAnimation");
+  setTimeout(() => {
+    body.classList.remove("perfectAnimation");
+  }, "500");  
+}
+
 const render_pos_display = (item) => {
   if (
     document.querySelectorAll("body > div > div.pos_display > p").length > 10
@@ -66,7 +113,7 @@ const render_pos_display = (item) => {
     clear_orders();
   }
   let order = document.createElement("p");
-  order.innerText = replace_(item) + ": " + menu[item].price;
+  order.innerText = replace_with_space(item) + ": " + menu[item].price;
   pos.appendChild(order);
 };
 
@@ -79,11 +126,24 @@ const animatePressed = (element) => {
 
 const addItem = (item) => {
   render_pos_display(item);
+  _inputed_Order.push(item);
 };
+
+const ai_order_to_array = (order_obj) => {
+  let returnArr = [];
+  for (item of order_obj) returnArr.push(replace_hyphen(item.imageName).slice(0, -4).toLowerCase());
+  return returnArr;
+}
 
 const place_order = () => {
   clear_orders();
-  add_ai_order();
+  let ai_order = ai_order_to_array(_ai_order);
+  let inputed_Order = [];
+  console.log("ai_order is: ", ai_order);
+  for (item of _inputed_Order) inputed_Order.push(replace_(item));
+  orders_are_equal(ai_order, inputed_Order) ? perfect() : tryAgain();
+  _inputed_Order = [];
+  _ai_order = add_ai_order();
 };
 
 const clear_orders = () => {
@@ -103,12 +163,9 @@ const create_ai_order = (num) => {
 };
 
 const get_random_int = () => {
-  // return a number between 3 and 5
-  // used for ai order's qty of items
+  // return a number between 3 and 5 for ai order's qty of items
   return Math.floor(Math.random() * 3) + 3;
 };
-
-const replace_ = (s) => s.split("_").join(" ");
 
 const getRandomItem = () => {
   const keys = Object.keys(menu);
@@ -144,21 +201,14 @@ const add_ai_order = () => {
     image.setAttribute("src", `images\\${img_src}`);
     ai_order_block.appendChild(image);
   });
+
+  return order;
+
 };
 
 const orders_are_equal = (ai_order, pos_input_order) => {
-  ai_order = Object.keys[ai_order].sort();
-  pos_input_order = Object.keys[pos_input_order].sort();
-
-  if (ai_order.length !== pos_input_order.length) {
-    return false;
-  }
-  for (let i = 0; i < ai_orders.length; i++) {
-    if (ai_order[i] !== pos_input_order[i]) {
-      return false;
-    }
-  }
-  return true;
+  console.log(ai_order, pos_input_order);
+  return (ai_order.equals(pos_input_order));
 };
 
-add_ai_order();
+let _ai_order = add_ai_order();
